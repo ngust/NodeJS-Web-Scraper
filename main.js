af4a0339bbc535c8c54ca533f5ec79c8ad40ed02
Nicholas Gust <nicholasgust@gmail.com>
2016-02-16 12:59:19 -0800

    getting urls from substack

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var url = 'http://www.whatsmyip.org';
+var url = 'http://substack.net/images/';
 var page;
 
 request(url, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(body);
-    console.log($('#ip').text());
+    // console.log($('#ip').text());
 
-
+    // var write = fs.createWriteStream("content.csv");
+    $('a').each(function(i,elem) {
+      console.log("i ",i);
+      console.log("http://substack.net" + elem.attribs['href']);
+    });
   }
 })
 
