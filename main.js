726bf048ff2b77125cbad9274937f59b40e6a495
Nicholas Gust <nicholasgust@gmail.com>
2016-02-16 17:01:56 -0800

    got data to write to CSV

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var cheerio = require('cheerio');
-
+var fs = require('fs');
 var url = 'http://substack.net/images/';
 var page;
 
@@ -9,18 +9,22 @@ request(url, function (error, response, body) {
     var $ = cheerio.load(body);
     // console.log($('#ip').text());
 
-    // var write = fs.createWriteStream("content.csv");
+    var stream = fs.createWriteStream("content.csv");
+    stream.once('open', function(fd) {
 
-    $('tr').each(function (){
-      var row = $(this);
-      var permissions = row.find('code').first().text();
-      var url = "http://substack.net/" + row.find('a').text();
-      var filetype = url.split('.').pop();
-      if (/.+\/$/.test(filetype)) {
-        filetype = "dir"
-      }
-      console.log(permissions, url, filetype);
-      });
+      $('tr').each(function (){
+        var row = $(this);
+        var permissions = row.find('code').first().text();
+        var url = "http://substack.net/" + row.find('a').text();
+        var filetype = url.split('.').pop();
+        if (/.+\/$/.test(filetype)) {
+          filetype = "dir"
+        }
+        console.log(permissions, url, filetype);
+        stream.write(permissions + "," + url + "," + filetype + '\n');
+        });
+    stream.end();
+    });
   }
 })
 
